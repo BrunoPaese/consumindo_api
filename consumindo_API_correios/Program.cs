da71cea0f6b46b89d3a454ee6a24c1d7cd0af936
Bruno Paese <brunoviniciuspaese@gmail.com>
2024-04-21 21:05:54 -0300

    Primeiro commit

---
+++ consumindo_API_correios/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Console.Write("Informe o CEP: ");
+        string cep = Console.ReadLine();
+
+        EnderecoService enderecoService = new EnderecoService();
+
+        Endereco enderecoEncontrado = await enderecoService.Integracao(cep);
+
+        if (!enderecoEncontrado.Verificacao) 
+        {
+            Console.WriteLine(enderecoEncontrado.ToString());
+        } else
+        {
+            Console.WriteLine($"CEP {cep} não encontrado");
+        }
+    } 
+}
---
+++ consumindo_API_correios/Service/EnderecoService.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+class EnderecoService
+{
+    public async Task<Endereco> Integracao(string cep)
+    {
+        HttpClient httpClient = new HttpClient();
+        var response = await httpClient.GetAsync($"https://viacep.com.br/ws/{cep}/json/");
+        var jsonString = await response.Content.ReadAsStringAsync();
+        var jsonObect = JsonConvert.DeserializeObject<Endereco>(jsonString);
+
+        if (jsonObect != null)
+        {
+            return jsonObect;
+        } else
+        {
+            return new Endereco(cep) {
+                Verificacao = true
+            };
+        }
+    }
+
+}
